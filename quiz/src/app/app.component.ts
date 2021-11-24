48a921942e4db684ceef03e8201858f58830b3b1
Arijus <79522941+nanthei@users.noreply.github.com>
2021-11-24 20:31:19 +0200

    firebase db updates

--- quiz/src/app/app.component.ts
+++ quiz/src/app/app.component.ts
@@ -9,16 +9,17 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   public title = 'quiz';
-
   public answers: Array<any> = [];
+
+  public quiz: any = {};
+  public quizQuestions: Array<any> = [];
+
   // Sukuriamas datos objekas
   public today: any = new Date();
   public currentQuestion : number = 0;
+
   // Kintamasis saugoti kiek % klausimu yra atsakyta
   public progress : number = 0;
-
-  // public answersFromDatabase : Observable<any[]>;
-
 
   constructor(db: AngularFireDatabase) {
     console.log("Siandienos data: ");
@@ -28,16 +29,20 @@ export class AppComponent {
     // https://angular.io/guide/observables-in-angular#async-pipe
     // this.answersFromDatabase = db.list('answers').valueChanges();
 
-    db.list('answers').valueChanges().subscribe((data : any ) => {
-      this.answers = data;
-      console.log(this.answers);
-    });
+    db.object('quizes/abc').valueChanges().subscribe((data : any ) => {
+      console.warn("Quiz data:");
+      this.quiz = data;
+      this.quizQuestions = data.questions;
 
+      console.warn(this.quizQuestions);
+    });
   }
 
   nextQuestion() {
     this.currentQuestion++;
-    this.progress = this.currentQuestion / this.answers.length  * 100;
+    let questionsCount = Object.keys(this.quizQuestions).length;
+
+    this.progress = (this.currentQuestion) / questionsCount  * 100;
     console.log("Progress: " + this.progress);
   }
 }
